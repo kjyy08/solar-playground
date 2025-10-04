7c410abf114a53e8bad78391ae3e4e68cbd617c6
kjyy08 <kjyy08@naver.com>
2025-10-05 03:23:36 +0900

    first commit

---
+++ src/test/java/cloud/luigi99/solar_playground/SolarPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.luigi99.solar_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
